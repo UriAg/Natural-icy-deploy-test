fd20a8ac5f467ab6186b7bad3248b7019b79f0b9
Uriel Agüero <uriel.aguero1812@gmail.com>
2024-02-19 21:41:38 -0300

    Agregadas etiquetas meta

--- src/router/menuNavigate.js
+++ src/router/menuNavigate.js
@@ -17,24 +17,24 @@ export const menuNavigate = [
         title: "Hamburguesas",
         icon: "ph:hamburger-bold"
     },
-    {
-        id: "frutasCongeladas",
-        path: "/category/frutas_congeladas",
-        title: "Frutas Congeladas",
-        icon: "fluent-emoji-high-contrast:strawberry"
-    },
+    // {
+    //     id: "frutasCongeladas",
+    //     path: "/category/frutas_congeladas",
+    //     title: "Frutas Congeladas",
+    //     icon: "fluent-emoji-high-contrast:strawberry"
+    // },
     {
         id: "sinTacc",
         path: "/category/sin_tacc",
         title: "Sin TACC",
         icon: "lucide:wheat"
     },
-    {
-        id: "verdurasCongeladas",
-        path: "/category/verduras_congeladas",
-        title: "Verduras Congeladas",
-        icon: "tabler:carrot"
-    },
+    // {
+    //     id: "verdurasCongeladas",
+    //     path: "/category/verduras_congeladas",
+    //     title: "Verduras Congeladas",
+    //     icon: "tabler:carrot"
+    // },
     {
         id: "batidos",
         path: "/category/batidos",
